87144e3c47c6ff1bb908abe3ed2482885116482b
kanger-007 <67798221+kanger-007@users.noreply.github.com>
2022-03-13 21:44:21 +0530

    Update dl.py

--- dl.py
+++ dl.py
@@ -46,7 +46,7 @@ subprocess.run(f'{mp4decrypt} --key {KEY} --show-progress Encrypted.Audio.m4a De
 subprocess.run(f'{mp4decrypt} --key {KEY} --show-progress Encrypted.Video.mp4 Decrypted.Video.mp4')
 print()
 print("\n Merging...")
-os.system('ffmpeg -i Decrypted_Video.mp4 -i Decrypted_Audio.m4a -c:v copy -c:a copy OUTPUT))
+os.system('ffmpeg -i Decrypted_Video.mp4 -i Decrypted_Audio.m4a -c:v copy -c:a copy (OUTPUT))
 
 
 
